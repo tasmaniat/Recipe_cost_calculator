41bbbb6684102a8591f7f4f36ad23ddb17639aeb
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2024-04-08 09:27:06 +1200

    works as expected

---
+++ c_04_get_unit.py
@@ -0,0 +1,39 @@
+def get_unit(question):
+    units = ["", "g", "gram", "grams", "kg", "kilogram",
+             "kilograms", "mg", "milligram", "milligrams",
+             "l", "liter", "liters"]  # Allowed units
+    unit_num = {
+        "g": 1,
+        "kg": 1000,
+        "mg": 0,
+        "l": 1
+    }
+    valid = False
+    while not valid:
+        unit = input(question).lower()
+        if unit in units:
+            return unit
+
+        # check for exit code...
+        elif unit == "xxx":
+            return unit
+
+        else:
+
+            print("\nPlease enter either: (g,kg,mg,l)"
+                  "\ngram, kilogram, milligram, Liter")
+            print()
+
+
+# Main Routine goes here...
+
+# Loop for testing purposes
+user_choice = ""
+while user_choice != "xxx":
+    # ASk user for choice and check it's valid
+    user_choice = get_unit("Unit? ( g, kg, mg, L, "
+                           "or leave blank): ")
+
+    # print out choice for comparison purposes
+    print("You chose {}".format(user_choice))
+    print()
