e840a4d775b462698463ae40297a6ff855928a88
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2024-05-07 21:44:19 +1200

    works as expected

--- c_03_variable_cost_v1.py
+++ c_03_variable_cost_v1.py
@@ -1,114 +1,88 @@
 import pandas
 
 
-# checks users enter an integer to a given question
-def num_check(question, error, num_type):
+# Function to check if the user input is an integer or a float, as required
+def num_check(question, error, num_type=float):
     valid = False
     while not valid:
-
         try:
             response = num_type(input(question))
-
             if response <= 0:
                 print(error)
             else:
                 return response
-
         except ValueError:
             print(error)
 
 
-# checks that user response is not blank
+# Function to ensure the user input is not blank
 def not_blank(question, error):
     valid = False
     while not valid:
         response = input(question)
-
         if response == "":
             print("{}. \nPlease try again \n".format(error))
             continue
-
         return response
 
 
-# currency formatting function
+# Function to format numbers as currency
 def currency(x):
     return "${:.2f}".format(x)
 
 
-# Gets expenses, returns list which has
-# the data frame and sub_total
-def get_expenses(var_fixed):
-    #  set up dictionaries and lists
-
+# Function to gather the recipe ingredients and calculate the total cost and cost per serving
+def get_ingredients():
     item_list = []
     quantity_list = []
-    price_list = []
+    cost_list = []
 
-    variable_dict = {
-        "List ingredient": item_list,
-        "Amount of ingredient": quantity_list,
-        "Cost of ingredient": price_list
-    }
-
-    # loop to get component, quantity adn price
-    item_name = ""
-    while item_name.lower() != "xxx":
-
+    ingredient_name = ""
+    while True:
         print()
-        # get name, quantity and item
-        item_name = not_blank("list ingredient:  ",
-                              "Can't be blank")
-
-        if item_name.lower() == "xxx":
+        ingredient_name = not_blank("Ingredient name : ",
+                                    "The ingredient name can't be blank.")
+        if ingredient_name.lower() == "xxx":
             break
 
-        quantity = num_check("The amount of ingredient: ",
-                             "The amount must be a whole number"
-                             "more than zero",
-                             int)
+        amount_needed = num_check("Amount needed : ",
+                                  "The amount must be a number more than zero")
 
-        price = num_check("Cost of ingredient: ",
-                          "The price must be a number <more "
-                          "than 0>", float)
+        cost = num_check("Cost for the specified amount ($): ",
+                         "The cost must be a number more than 0")
 
-        # add item, quantity and price to lists
-        item_list.append(item_name)
-        quantity_list.append(quantity)
-        price_list.append(price)
+        item_list.append(ingredient_name)
+        quantity_list.append(amount_needed)
+        cost_list.append(cost)
 
-    expense_frame = pandas.DataFrame(variable_dict)
-    expense_frame = expense_frame.set_index('Ingredient')
+    ingredient_dict = {
+        "Ingredient": item_list,
+        "Amount needed": quantity_list,
+        "Cost": cost_list
+    }
 
-    # Calculate cost of each component
-    expense_frame['Cost'] = expense_frame['Quantity'] * expense_frame['Price']
+    ingredient_frame = pandas.DataFrame(ingredient_dict)
+    ingredient_frame = ingredient_frame.set_index('Ingredient')
 
-    # Find sub_total
-    sub_total = expense_frame['Cost'].sum()
+    # Calculate total cost
+    total_cost = ingredient_frame['Cost'].sum()
 
-    # Currency Formatting (uses currency function)
-    add_dollars = ['Price', 'Cost']
-    for item in add_dollars:
-        expense_frame[item] = expense_frame[item].apply(currency)
-
-    return [expense_frame, sub_total]
+    return [ingredient_frame, total_cost]
 
 
-# ******* main routine goes here *******
+# Main routine
+# recipe_name = not_blank("Recipe name: ", "The recipe name can't be blank.")
+# serving_size = num_check("Serving size: ", "The serving size must be an integer more than zero", int)
 
-# Get user data
-unit_list = ["g", "kg", "mg", "L", ""]
+ingredient_expenses = get_ingredients()
+ingredient_frame = ingredient_expenses[0]
+total_cost = ingredient_expenses[1]
+# cost_per_serving = total_cost / serving_size
 
-product_name = not_blank("Recipe name: ", "The Recipe name can't be blank. ")
-
-variable_expenses = get_expenses("variable")
-variable_frame = variable_expenses[0]
-variable_sub = variable_expenses[1]
-
-# ***** Printing Area *****
-
+# Printing the results
 print()
-print(variable_frame)
+# print(f"Recipe: {recipe_name}")
+print(ingredient_frame)
 print()
-
-print("Fixed Costs: ${:.2f}".format(variable_sub))
+print(f"Total cost: {currency(total_cost)}")
+# print(f"Cost per serving (for {serving_size} servings): {currency(cost_per_serving)}")
